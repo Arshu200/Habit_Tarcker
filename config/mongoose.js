a85146d871dc91a220870c24b61b81f918dcbd5b
Arshad <91214096+Arshu200@users.noreply.github.com>
2024-07-05 11:40:19 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // storing the db on mongo atlas
-const DB = "mongodb+srv://shaik22arshad:iWb1774yZUPwZ3ki@cluster0.ntipqh2.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://shaik22arshad:ZABKm2Cx8rIt9XDF@cluster0.jthsizw.mongodb.net/?retryWrites=true&w=majority";
 
 // mongoose.connect('mongodb://127.0.0.1/habit_tracker');
 
